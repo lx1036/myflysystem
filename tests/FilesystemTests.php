389abd89284c5b1ed7e0879377f6c3ede8691f4f
Xiang Liu <xiang.liu@rightcapital.com>
2016-09-05 22:05:23 +0800

    Update filesystem test

--- tests/FilesystemTests.php
+++ tests/FilesystemTests.php
@@ -8,12 +8,47 @@
 
 namespace RightCapital\FlySystem\FlySystem\Test;
 
+use Prophecy\Argument;
 use RightCapital\FlySystem\FlySystem\Adapter\Local;
+use RightCapital\FlySystem\FlySystem\AdapterInterface;
 use RightCapital\FlySystem\FlySystem\Cache\Memory;
+use RightCapital\FlySystem\FlySystem\Config\Config;
 use RightCapital\FlySystem\FlySystem\Filesystem;
 
 class FilesystemTests extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var \Prophecy\Prophecy\ObjectProphecy
+     */
+    protected $prophecy;
+    /**
+     * @var AdapterInterface
+     */
+    protected $adapter;
+    /**
+     * @var Config
+     */
+    protected $filesystemConfig;
+    /**
+     * @var Filesystem
+     */
+    protected $filesystem;
+    /**
+     * @var Argument\Token\TypeToken
+     */
+    protected $config;
+
+    /**
+     * @before
+     */
+    public function setupAdapter()
+    {
+        $this->prophecy = $this->prophesize(AdapterInterface::class);
+        $this->adapter = $this->prophecy->reveal();
+        $this->filesystemConfig = new Config();
+        $this->filesystem = new Filesystem($this->adapter, $this->filesystemConfig);
+        $this->config = Argument::type(Config::class);
+    }
     public function testInstantiable()
     {
         $instance = new Filesystem($adapter = new Local(__DIR__ . '/../resources/'), $cache = new Memory());
